27cac644eb2d41423ac50a3a6b0b149e41a475dc
kevinkalkan <kevinkalkan98@outlook.com>
2023-11-14 22:19:26 +0100

    Added other pages, modified screenshot

--- main.py
+++ main.py
@@ -1,40 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from teachable_machine import TeachableMachine
-import cv2 as cv
 import time
+import cv2 as cv
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
-model = TeachableMachine(model_path="keras_model.h5",
-                         labels_file_path="labels.txt")
+model = TeachableMachine(model_path="keras_model.h5", labels_file_path="labels.txt")
 
+CONSTANT_IMAGE_FILE = "static/screenshot.jpg"
 cap = cv.VideoCapture(0)
+
 
 def start_video_capture():
     _, img = cap.read()
-    timestamp = int(time.time())
-    image_path = f"screenshot_{timestamp}.jpg"
-    cv.imwrite(image_path, img)
-    result = model.classify_image(image_path)
-    print("class_index", result["class_index"])
-    print("class_name:::", result["class_name"])
-    return result["class_name"]
 
-# Render the HTML page
+    # Overwrite the existing screenshot file with the new frame
+    cv.imwrite(CONSTANT_IMAGE_FILE, img)
+
+    result = model.classify_image(CONSTANT_IMAGE_FILE)
+    class_name = result["class_name"]
+
+    print("class_index", result["class_index"])
+    print("class_name:::", class_name)
+
+    return class_name
+
+
+def stop_video_capture():
+    cap.release()
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/classify', methods=['GET'])
 def classify():
-    global stored_class_name
-    stored_class_name = start_video_capture()
-    return stored_class_name
-    
+    class_name = start_video_capture()
+    return class_name
+
+
 @app.route('/stop')
 def stop():
+    stop_video_capture()
     cv.destroyAllWindows()
-    return 'Video stream stopped.'
+    return redirect('/')
+
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
+
+
+@app.route('/screen')
+def screen():
+    return render_template('screen.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
